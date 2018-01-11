457bc3859a7cd7d4e0fd0a57ca6b6080e2159322
AILLERON\voma <voma@ailleron.com>
2018-01-11 17:15:12 +0100

    update lesson 3-5

--- ToddProAng5/src/app/auth-form/auth-form.component.ts
+++ ToddProAng5/src/app/auth-form/auth-form.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ContentChild, AfterContentInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthForm } from './auth-form.interface';
+import { AuthRememberComponent } from '../auth-remember/auth-remember.component';
 @Component({
   selector: 'app-auth-form',
   templateUrl: './auth-form.component.html',
   styleUrls: ['./auth-form.component.scss']
 })
-export class AuthFormComponent implements OnInit {
+export class AuthFormComponent implements AfterContentInit {
 
   // submitted: boolean = false;
+  showMessage: boolean;
+
+  @ContentChild(AuthRememberComponent) remember: AuthRememberComponent;
 
   @Output() submitted: EventEmitter<string> = new EventEmitter<string>();
 
   constructor() {}
+
+  ngAfterContentInit() {
+    if (this.remember) {
+      this.remember.checked.subscribe((checked: boolean) => {
+        this.showMessage = checked;
+      })
+    }
+  }
 
   // rememberMe: boolean = false;
   //
@@ -25,8 +37,5 @@ export class AuthFormComponent implements OnInit {
     // console.log(t)
   }
 
-  ngOnInit() {
-
-  }
 
 }
